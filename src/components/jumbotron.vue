05b2dc378830937bd94494208fbc23a81fa2eed4
DavideZattra <d.zattra@gmail.com>
2021-09-23 15:56:37 +0200

    added content and button to jumbotron

--- src/components/jumbotron.vue
+++ src/components/jumbotron.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="ms_jumbotron">
+  <div id="ms_jumbotron">
     
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus odio nemo ea officiis temporibus sequi, molestiae hic quas eos voluptatum quibusdam fuga fugit illo repellat tempora aliquid molestias nulla alias!</p>
+    <button>Lorem ipsum</button>
   </div>
 </template>
 
@@ -29,4 +31,21 @@ li {
 a {
   color: #42b983;
 }
+
+button{
+
+    border-radius: 10px;
+    font-weight: 600;
+    font-size: 1rem;
+    color: white;
+    background-color: grey;
+    
+}
+
+#ms_jumbotron{
+    margin: 0 auto;
+    width: 600px;
+}
+
+
 </style>
