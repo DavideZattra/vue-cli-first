44750fe734bd8b1dd9fac8d2d01da8c4bd1e3cd5
DavideZattra <d.zattra@gmail.com>
2021-09-23 15:32:39 +0200

    initialized vue-cli-first

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <Jumbotron />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/header'
+import Jumbotron from './components/jumbotron.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Jumbotron
   }
 }
 </script>
